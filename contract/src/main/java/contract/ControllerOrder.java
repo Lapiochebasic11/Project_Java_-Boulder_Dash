8544662cf63df466d989def26731778add10658b
super685 <pierreolivier.schaller@viacesi.fr>
2019-05-27 16:13:01 +0200

    Vue + model corrige

--- contract/src/main/java/contract/ControllerOrder.java
+++ contract/src/main/java/contract/ControllerOrder.java
@@ -5,22 +5,8 @@ package contract;
  *
  * @author Jean-Aymeric Diet
  */
-<<<<<<< HEAD
-public enum ControllerOrder {
 
-	/** move Up. */
-	Up,
-	/** move left. */
-	Left,
-	/** move right. */
-	Right,
-	/** move down. */
-	Down,
-	/** nothing. */
-	Nothing,
-=======
 public enum ControllerOrder 
 {
 	UP, DOWN, LEFT, RIGHT
->>>>>>> branch 'master' of https://github.com/Lapiochebasic11/Project_Java_-Boulder_Dash
 }
